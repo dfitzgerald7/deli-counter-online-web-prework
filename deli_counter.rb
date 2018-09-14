4d12cd242475db929562a0d412018ed30d1757c8
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-14 16:07:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def line(array)
+  if array.size < 1 
+    puts "The line is currently empty."
+  else 
+    myOutput = "The line is currently: "
+    array.each.with_index do |
+  
+  end 
+  
+end 
